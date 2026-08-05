047dceca0e91ff22e2f4fd6f0b0a3eae6eae00e7
Muhammad Faizan Rana <ranafaizan.asap@gmail.com>
2026-08-05 05:33:59 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 ext_modules = [
     Pybind11Extension(
         "job_matcher",
-        ["job_matcher.cpp"], 
+    ["cpp_engine/job_matcher.cpp"],
     ),
 ]
 
