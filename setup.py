2749807ebf21ce3dd28a572bf3bcd25e343b9cd0
Muhammad Faizan Rana <ranafaizan.asap@gmail.com>
2026-08-05 05:32:05 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        "job_matcher",
+        ["job_matcher.cpp"], 
+    ),
+]
+
+setup(
+    name="job_matcher",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
